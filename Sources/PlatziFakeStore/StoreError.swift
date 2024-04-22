ec66645a8f0ab099370d4daccf4651a4a69951a3
Ilia Shapovalov <greateman@mail.ru>
2024-04-22 16:21:54 +0300

    Добалена документация для ошибок. Рефакторинг.

--- Sources/PlatziFakeStore/StoreError.swift
+++ Sources/PlatziFakeStore/StoreError.swift
@@ -7,10 +7,29 @@
 
 import Foundation
 
+/// Возможные ошибки, которые могу возникнут в ходе запроса.
 public enum StoreError: Error, Equatable {
+    
+    /// Любой сценарий, не вписывающийся в остальные ошибки попадает сюда.
     case unknown
+    
+    /// Ошибка указывает на некорректно сформированный запрос.
+    ///
+    /// Ошибка `StoreError.badRequest` возникает по следующим причинам:
+    /// - В запросах на поиск, добавление, обновление и удаление.
+    /// Если сущность с указанным идентификатором не найдена в базе.
+    /// - В запросе на создание или обновлении сущности,  неправильно заполнены поля модели.
     case badRequest(String)
+    
+    /// Возникает в запросах, связанных с сессией пользователя
     case unauthorized
     
-    init(_ error: Error) { self = .unknown }
+    init(_ error: Error) {
+        switch error {
+        case let storeError as Self:
+            self = storeError
+            
+        default: self = .unknown
+        }
+    }
 }
--- Tests/PlatziFakeStoreTests/StoreErrorTests.swift
+++ Tests/PlatziFakeStoreTests/StoreErrorTests.swift
@@ -12,6 +12,12 @@ final class StoreErrorTests: XCTestCase {
     private var response: HTTPURLResponse!
     private let expectation = XCTestExpectation()
     
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        response = nil
+    }
+    
     func test_unknownError() {
         let sut = PlatziFakeStore { _ in .failure(URLError(.unknown)) }
         
@@ -28,13 +34,40 @@ final class StoreErrorTests: XCTestCase {
         wait(for: [expectation], timeout: 0.1)
     }
     
+    func test_statusCode_200_success() {
+        response = makeRequest(statusCode: 200)
+        let data = true.description.data(using: .utf8)!
+        let sut = PlatziFakeStore { _ in .success((data, self.response)) }
+        
+        sut.deleteProduct(withId: 1) { result in
+            self.expectation.fulfill()
+            switch result {
+            case .success(let deleted): XCTAssertTrue(deleted)
+            case .failure:  XCTFail()
+            }
+        }
+        
+        wait(for: [expectation], timeout: 0.1)
+    }
+    
+    func test_statusCode_201_success() {
+        response = makeRequest(statusCode: 201)
+        let data = true.description.data(using: .utf8)!
+        let sut = PlatziFakeStore { _ in .success((data, self.response)) }
+        
+        sut.deleteProduct(withId: 1) { result in
+            self.expectation.fulfill()
+            switch result {
+            case .success(let deleted): XCTAssertTrue(deleted)
+            case .failure:  XCTFail()
+            }
+        }
+        
+        wait(for: [expectation], timeout: 0.1)
+    }
+    
     func test_badRequestError() {
-        response = HTTPURLResponse(
-            url: URL(string: "baz")!,
-            statusCode: 401,
-            httpVersion: nil,
-            headerFields: nil
-        )
+        response = makeRequest(statusCode: 400)
         let sut = PlatziFakeStore { _ in .success((Data(), self.response)) }
         
         sut.productList { result in
@@ -46,5 +79,33 @@ final class StoreErrorTests: XCTestCase {
         }
         
         wait(for: [expectation], timeout: 0.1)
+    }
+    
+    func test_unauthorizedError() {
+        response = makeRequest(statusCode: 401)
+        let sut = PlatziFakeStore { _ in .success((Data(), self.response)) }
+        
+        sut.productList { result in
+            self.expectation.fulfill()
+            switch result {
+            case .success: XCTFail()
+            case .failure(let error): XCTAssertEqual(error, .unauthorized)
+            }
+        }
+        
+        wait(for: [expectation], timeout: 0.1)
+    }
+    
+//    func test_
+}
+
+private extension StoreErrorTests {
+    func makeRequest(statusCode: Int) -> HTTPURLResponse? {
+        HTTPURLResponse(
+            url: URL(string: "baz")!,
+            statusCode: statusCode,
+            httpVersion: nil,
+            headerFields: nil
+        )
     }
 }
